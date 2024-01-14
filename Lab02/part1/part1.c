590b39ec11f370d9842cb16cc9cf0e4d0e54ba83
sagarwal <shubhagarwa8888@gmail.com>
2024-01-14 23:14:31 +0530

    part1 files added

---
+++ Lab02/part1/part1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    char message[] = "Hello World";
+    for (int i = 0; i < 11; i++) {
+		printf("%c %d\n", message[i], getpid());
+		sleep(rand() % 4 + 1);
+		if (fork() != 0){
+            wait(NULL);
+			break;
+		}
+	}
+	return 0;
+}
