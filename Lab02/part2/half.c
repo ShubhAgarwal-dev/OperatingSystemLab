2c38519c9e355eb461c0d903ccc9e473be97cd59
sagarwal <shubhagarwa8888@gmail.com>
2024-01-15 18:41:35 +0530

    addded the required files

--- Lab02/part2/half.c
+++ Lab02/part2/half.c
@@ -7,60 +7,28 @@ int foo(int ele) {
     return ele/2;
 }
 
-void tostring(char str[], int num)
-{
-    int i, rem, len = 0, n;
- 
-    n = num;
-    while (n != 0)
-    {
-        len++;
-        n /= 10;
-    }
-    for (i = 0; i < len; i++)
-    {
-        rem = num % 10;
-        num = num / 10;
-        str[len - (i + 1)] = rem + '0';
-    }
-    str[len] = '\0';
-}
-
 int main(int argc, char* argv[]) {
     if (argc <= 1) {
         fprintf(stderr, "Impropper number of arguments");
         exit (1);
     }
 
-    // printf("%d\n", argc);
-    // for (int i=0; i<argc; i++){
-    //     printf("%s\n", argv[i]);
-    // }
-
     int num = atoi(argv[argc-1]);
-    // printf("Number is: %d\n", num);
 
     int res = foo(num);
-    printf("Current process id: %d, Current result: %d", (int) getpid(), res);
+    printf("Current process id: %d, Current result: %d\n", (int) getpid(), res);
 
-    char *argument_list[argc-1];
-    for (int i=1; i<argc; i++) {
-        argument_list[i-1] = argv[i];
+    sprintf(argv[argc-1], "%d", res);
+
+    int status_code;
+    if (argc > 2){
+        status_code = execvp(argv[1], argv+1);
+    } else {
+        return 0;
     }
-
-    argument_list[argc-1] = NULL;
-    // printf("Working till here\n");
-    tostring(argument_list[argc-2], res);
-
-    for (int i=0; i<argc; i++){
-        printf("%s\n", argument_list[i]);
-    }
-    printf("\n\n\n");
-
-    int status_code = execvp(argument_list[0], argument_list);
 
     if (status_code == -1) {
-        printf("Terminated Incorrectly\n");
+        printf("Execution Complete\n");
         return 1;
     }
 
--- Lab02/part2/square.c
+++ Lab02/part2/square.c
@@ -7,61 +7,29 @@ int foo(int ele) {
     return ele*ele;
 }
 
-void tostring(char str[], int num)
-{
-    int i, rem, len = 0, n;
- 
-    n = num;
-    while (n != 0)
-    {
-        len++;
-        n /= 10;
-    }
-    for (i = 0; i < len; i++)
-    {
-        rem = num % 10;
-        num = num / 10;
-        str[len - (i + 1)] = rem + '0';
-    }
-    str[len] = '\0';
-}
-
 int main(int argc, char* argv[]) {
     if (argc <= 1) {
         fprintf(stderr, "Impropper number of arguments");
         exit (1);
     }
 
-    printf("%d\n", argc);
-    for (int i=0; i<argc; i++){
-        printf("%s\n", argv[i]);
-    }
-
     int num = atoi(argv[argc-1]);
-    printf("Number is: %d\n", num);
 
     int res = foo(num);
-    printf("Current process id: %d, Current result: %d", (int) getpid(), res);
+    printf("Current process id: %d, Current result: %d\n", (int) getpid(), res);
 
-    char *argument_list[argc-1];
-    for (int i=1; i<argc; i++) {
-        argument_list[i-1] = argv[i];
+    sprintf(argv[argc-1], "%d", res);
+
+    int status_code;
+    if (argc > 2){
+        status_code = execvp(argv[1], argv+1);
+    } else {
+        return 0;
     }
-
-    argument_list[argc-1] = NULL;
-    // printf("Working till here\n");
-    tostring(argument_list[argc-2], res);
-
-    for (int i=0; i<argc; i++){
-        printf("%s\n", argument_list[i]);
-    }
-    printf("\n\n\n");
-
-    int status_code = execvp(argument_list[0], argument_list);
 
     if (status_code == -1) {
-        printf("Terminated Incorrectly\n");
-        return 0;
+        printf("Execution Complete\n");
+        return 1;
     }
 
     printf("This should not be printed\n");
